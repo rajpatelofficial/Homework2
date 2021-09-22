af00f0ca23cab5e66b1fc4b7e77c9ba6f135a257
Raj Patel <91072711+rajpatelofficial@users.noreply.github.com>
2021-09-22 08:53:50 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CompilerHomework
+//
+//  Created by Admin on 9/22/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
