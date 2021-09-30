38e54ee6f45bc8116de1337d782617cd95ea0f3f
rajpatelofficial <rapatel@noctrl.edu>
2021-09-30 15:16:21 -0500

    update operator from source file

--- CompilerHomework/main.cpp
+++ CompilerHomework/main.cpp
@@ -18,28 +18,52 @@ private:
     vector<string> tokens;   // source code file tokens
     map<string, string> tokenmap;  // valid lexeme/token pairs
     // other private methods
+    
 public:
-
     LexAnalyzer(istream& infile){
 // pre: parameter refers to open data file consisting of token and
 // lexeme pairs i.e.  s_and and t_begin begin t_int 27.  Each pair    // appears on its own input line.
 // post: tokenmap has been populated
     
-    string lexeme;
-    string token;
-    for(int i = 0;i<32;i++){
-        infile>>lexeme;
-        infile>>token;
-        tokenmap[lexeme]= token;
+        string lexeme;
+        string token;
+        while(!infile.eof()){
+            infile>>lexeme;
+            infile>>token;
+            tokenmap[lexeme]= token;
     }
         map<string,string>::iterator itr;
         for(itr = tokenmap.begin(); itr != tokenmap.end();itr++){
-            //cout<<itr->first<< " : "<<itr->second<<endl;
-        }
-    };
+           // cout<<itr->first<< " : "<<itr->second<<endl;
+    }
+    }
 
-void scanFile(istream& infile, ostream& outfile);
-    
+    void scanFile(istream& infile, ostream& outfile){;
+        char ch;
+        char operators[] = "+-*/%";
+        int i = 0;
+        
+        while(!infile.eof()){
+           ch = infile.get();
+            
+            for(i=0;i<6;i++){
+                if(ch == operators[i] && operators[i] == '+')
+                    cout<<"s_plus : "<<ch<<endl;
+                else if(ch == operators[i] && operators[i] == '-')
+                    cout<<"s_minus : "<<ch<<endl;
+                else if(ch == operators[i] && operators[i] == '*')
+                    cout<<"s_mult : "<<ch<<endl;
+                else if(ch == operators[i] && operators[i] == '/')
+                    cout<<"s_div : "<<ch<<endl;
+                else if(ch == operators[i] && operators[i] == '%')
+                    cout<<"s_mod : "<<ch<<endl;
+                    
+            }
+            
+            
+        }//end while
+        
+    }
 };
 
 
@@ -53,8 +77,25 @@ int main(int argc, const char * argv[]) {
         exit(-1);
     }
     
+    ifstream infileSource("source.txt");
+    
+    if(!infileSource){
+        cout<<"unable to get file"<<endl;
+        exit(-1);
+    }
+    
+    ofstream outfile("Result");
+    if(!outfile){
+        cout<<"Error: File can't be opened"<<endl;
+        exit(-1);
+    }
     
     LexAnalyzer lex(infile);
+    lex.scanFile(infileSource, outfile);
+    
+    
     
     return 0;
-}
+};
+
+
