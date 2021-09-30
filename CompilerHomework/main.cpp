a03908d12f9fd01318704b8d1b4734f0b4074f49
rajpatelofficial <rapatel@noctrl.edu>
2021-09-30 09:47:11 -0500

    test

--- CompilerHomework/main.cpp
+++ CompilerHomework/main.cpp
@@ -20,7 +20,7 @@ private:
     // other private methods
 public:
 
-    LexAnalyzer(istream& infile){;
+    LexAnalyzer(istream& infile){
 // pre: parameter refers to open data file consisting of token and
 // lexeme pairs i.e.  s_and and t_begin begin t_int 27.  Each pair    // appears on its own input line.
 // post: tokenmap has been populated
@@ -34,12 +34,12 @@ public:
     }
         map<string,string>::iterator itr;
         for(itr = tokenmap.begin(); itr != tokenmap.end();itr++){
-            cout<<itr->first<< " : "<<itr->second<<endl;
+            //cout<<itr->first<< " : "<<itr->second<<endl;
         }
-    }
+    };
 
 void scanFile(istream& infile, ostream& outfile);
-
+    
 };
 
 
@@ -52,7 +52,9 @@ int main(int argc, const char * argv[]) {
         cout<<"unable to get file"<<endl;
         exit(-1);
     }
-    (LexAnalyzer(infile));
+    
+    
+    LexAnalyzer lex(infile);
     
     return 0;
 }
